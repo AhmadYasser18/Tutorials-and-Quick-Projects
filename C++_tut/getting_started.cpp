e366c6b163b3a18ad3f4768fac30e1237b2f3e7c
Ahmad <ahmad2000yasser@gmail.com>
2025-03-16 23:06:35 +0200

    adding folder for C++

---
+++ C++_tut/getting_started.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+/* main() function: where the execution of
+C++ program begins */
+int main() {
+  
+    // This statement prints "Hello World"
+    
+    /// cout << "Hello Ahmad";
+
+
+
+    return 0;
+}
