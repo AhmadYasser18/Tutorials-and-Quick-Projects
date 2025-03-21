794bc4634edfec3f33fb82195d3c64e7e7f43af2
Ahmad <ahmad2000yasser@gmail.com>
2025-03-21 19:25:28 +0200

    updating

--- C++_tut/getting_started.cpp
+++ C++_tut/getting_started.cpp
@@ -7,9 +7,50 @@ int main() {
   
     // This statement prints "Hello World"
     
-    /// cout << "Hello Ahmad";
+    cout << "Hello Ahmad"<<endl;
 
+    ///////////////////////
+    /*Variables:
+    There are two steps to create a ariable: declaration and assignment 
+    */
+   //integer (whole numbers)
+    int a;
+    a = 10;
 
+    int b = 5;
 
+    cout<<a<<endl<<b<<endl;
+    
+    int c;
+    c = 7.5; 
+    cout<<c; //prints as 7
+
+    //double (number including decimal)
+    double d;
+    d = 7.5;
+
+    cout<<' '<<d<<endl; //prints as 7.5
+
+    //char (single character)
+    char letter = 'a';
+
+    cout<<letter<<endl;
+
+    //boolean (true/false)
+
+    bool active = true;
+
+    cout<<"Is active: "<<active<<endl;
+
+    /*
+    string: - object representing sequence of text; 
+            - provided from 'std' namespace; 
+            - if not used then -> std::string var
+    */
+
+    string name = "Ahmad";
+    
+    cout<<"Hello "<<name<<endl;
+    
     return 0;
 }
