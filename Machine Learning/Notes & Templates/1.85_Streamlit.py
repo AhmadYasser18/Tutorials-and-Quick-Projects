0fae2098c7471a66f892002c8ce8b3ce06048061
Ahmad Yasser <73788070+AhmadYasser18@users.noreply.github.com>
2026-01-02 09:34:49 +0000

    completed 1.85

--- Machine Learning/Notes & Templates/1.85_Streamlit.py
+++ Machine Learning/Notes & Templates/1.85_Streamlit.py
@@ -1,8 +1,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import time
 import os
+from sklearn.datasets import load_iris
+
 
 import streamlit as st
 
@@ -16,6 +19,27 @@ st.line_chart(data)
 'Bar Graph:'
 
 st.bar_chart(data)
+###
+students = ['A', 'B', 'C']
+score = [100,75,25]
+attendance = [100,95,90]
+
+x = np.arange(len(score))
+width = 0.4
+
+fig, ax = plt.subplots()
+
+ax.bar(x-0.2, score, width, color='blue')
+ax.bar(x+0.2, attendance, width, color='black')
+
+ax.set_xticks(x)
+ax.set_xticklabels(students)
+
+ax.legend(['Score', 'Attendance'])
+
+
+st.pyplot(fig)
+
 #########
 
 Classes = ['A', 'B', 'C']
@@ -42,7 +66,21 @@ for i in range(1,100):
     #rows = new_
     
     time.sleep(0.05)
+#########
 
+iris_data = load_iris()
+data = pd.DataFrame(iris_data.data, columns = iris_data.feature_names)
 
+fig = plt.figure()
+sns.histplot(data = data, bins = 20)
+st.pyplot(fig)
+
+fig = plt.figure()
+sns.boxplot(data = data)
+st.pyplot(fig)
+
+fig = plt.figure()
+sns.scatterplot(data = data)
+st.pyplot(fig)
 
 #streamlit run /workspaces/Tutorials-and-Quick-Projects/"Machine Learning"/"Notes & Templates"/1.85_Streamlit.py
